01fd94dcfc1d3d223451230e3cbd380c8d7c5e85
AmeyPank <pankantiameya@gmail.com>
2023-05-16 00:37:46 +0530

    Flames-Do-DO

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Form from './Form.js';
 
-class App extends Component {
-    render() {
+const App = () => {
+    const [answer, setAnswer] = useState("");
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+    return(
+        <div id="main">
+           {/* Do not remove the main div */}
+           <Form 
+                setAnswer = {setAnswer}
+           />
+           <h3 data-testid="answer">{answer}</h3>
+        </div>
+    )
 }
 
 
